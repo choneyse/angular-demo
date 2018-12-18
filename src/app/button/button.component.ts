858a1d7b5a408d2c665e759fc8a35c095676934b
Chris Honeysett <chris.honeysett@vml.com>
2018-12-18 14:15:49 -0500

    Adding Button Component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { text } from '@angular/core/src/render3';
+
+@Component({
+  selector: 'fat-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  @Input() buttonHref;
+  @Input() buttonText;
+
+  ngOnInit() {}
+}
