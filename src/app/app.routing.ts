018f79324f62e7c89d3b7aacd9700f20b59672af
Chris Honeysett <chris.honeysett@vml.com>
2018-12-20 08:44:22 -0500

    Routing for individual components is working - animations need tweaking

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { FeaturedComponent } from './featured/featured.component';
+import { BeyondTheRainbowComponent } from './shows/beyond-the-rainbow/beyond-the-rainbow.component';
+import { ADollsHouseComponent } from './shows/a-dolls-house/a-dolls-house.component';
+import { MarvelousWonderettesComponent } from './shows/marvelous-wonderettes/marvelous-wonderettes.component';
 
 export const appRoutes: Routes = [
-  // { path: 'bubu', component: FeaturedComponent },
-  { path: ':show', component: FeaturedComponent },
+  // { path: ':show', component: FeaturedComponent },
+  { path: 'beyond-the-rainbow', component: BeyondTheRainbowComponent },
+  { path: 'a-dolls-house-part-2', component: ADollsHouseComponent },
+  { path: 'marvelous-wonderettes-dream-on', component: MarvelousWonderettesComponent },
   { path: '', pathMatch: 'full', redirectTo: 'beyond-the-rainbow' }
 ];
 
