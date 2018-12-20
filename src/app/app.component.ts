aa92cdb8329e4648b69b93da96d8e8577ce40864
Chris Honeysett <chris.honeysett@vml.com>
2018-12-20 13:23:00 -0500

    Featured transitions are now working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { fadeAnimation, fromLeft } from './animations/animations';
+import { fadeAnimation, fromLeft, featuredAnimation } from './animations/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [fromLeft]
+  animations: [featuredAnimation]
 })
 export class AppComponent {
   title = 'fat-angular';
