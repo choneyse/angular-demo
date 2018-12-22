e333c2080603003e7ab1e37380c36ecbaeca6b03
Chris Honeysett <aoy47ydb>
2018-12-22 14:07:47 -0500

    Changing nav to all jump links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,4 +9,18 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  public navigateTo(event) {
+    event.preventDefault();
+    const href: string = event.target.href;
+    const hash: string[] = href.split('#');
+    // document.getElementById(hash[1]).scrollIntoView({
+    //   behavior: 'smooth'
+    // });
+    const element = document.getElementById(hash[1]);
+    window.scroll({
+      top: element.offsetTop - 80,
+      behavior: 'smooth'
+    });
+  }
 }
